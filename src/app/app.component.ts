735493f259bb1cd59e72f3e299caa5bf32c44218
bersling <bersling@gmail.com>
2018-05-02 00:08:00 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+    cards = [{
+        title: 'Card 1',
+        content: 'Bla jajdlk faslkjklfdas jklds aljjlkafs kasfdj kasd jlkjlkfda s'
+    }, {
+        title: 'Card 2',
+        content: 'afdkljj lljalsjfd jaklds adklfs jkldasf jljlkad slalasdfjk '
+    }, {
+        title: 'Card 3',
+        content: 'ajlkdsf jlkadsjkl jklad sjlkjaklf jajd jdf jkl ajlkj lkajk fldsjlkdsfa jjlkasf '
+    }, ];
+
 }
