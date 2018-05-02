cde0343d66a2daaf974639220c90c91c41f605a1
bersling <bersling@gmail.com>
2018-05-02 08:23:51 +0200

    some more work on the cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,16 @@ export class AppComponent {
 
     cards = [{
         title: 'Card 1',
-        content: 'Bla jajdlk faslkjklfdas jklds aljjlkafs kasfdj kasd jlkjlkfda s'
+        content: 'Bla jajdlk faslkjklfdas jklds aljjlkafs kasfdj kasd jlkjlkfda s',
+        icon: '/assets/001-robot-arm.svg'
     }, {
         title: 'Card 2',
-        content: 'afdkljj lljalsjfd jaklds adklfs jkldasf jljlkad slalasdfjk '
+        content: 'afdkljj lljalsjfd jaklds adklfs jkldasf jljlkad slalasdfjk ',
+        icon: '/assets/002-cyborg.svg'
     }, {
         title: 'Card 3',
-        content: 'ajlkdsf jlkadsjkl jklad sjlkjaklf jajd jdf jkl ajlkj lkajk fldsjlkdsfa jjlkasf '
+        content: 'ajlkdsf jlkadsjkl jklad sjlkjaklf jajd jdf jkl ajlkj lkajk fldsjlkdsfa jjlkasf ',
+        icon: '/assets/003-robot-1.svg'
     }, ];
 
 }
